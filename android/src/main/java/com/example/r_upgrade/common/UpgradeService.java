e2c9142c1021d8a80ce0660cf93d98db40606ded
Hiperion <hiperioncn@users.noreply.github.com>
2024-07-09 09:53:27 +0800

    Update UpgradeService.java
    修复android14无法使用问题

--- android/src/main/java/com/example/r_upgrade/common/UpgradeService.java
+++ android/src/main/java/com/example/r_upgrade/common/UpgradeService.java
@@ -133,7 +133,12 @@ public class UpgradeService extends Service {
         } else {
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         }
-        registerReceiver(actionReceiver, filter);
+        //registerReceiver(actionReceiver, filter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(actionReceiver, filter, Context.RECEIVER_EXPORTED);
+        } else {
+            registerReceiver(actionReceiver, filter);
+        }
 
     }
 
